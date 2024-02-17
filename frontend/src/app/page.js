5a80b0824129b35adf5d316ff287921c98739b3b
43 <fffatal0@gmail.com>
2024-02-17 07:03:50 +0300

    fetch entity

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,29 @@
-export default function Home() {
-  return (
-    <h1 className="text-center">Hello</h1>
-  );
+const API_URL = process.env.API_URL
+const API_TOKEN = process.env.API_TOKEN
+
+export default async function Home() {
+    const res = await fetch(`${API_URL}/api/news?populate=preview`, {
+        headers: {
+            authorization: `bearer ${API_TOKEN}`
+        }
+    })
+
+    const {data} = await res.json()
+
+    console.log(data[1].attributes.preview.data.attributes.url)
+
+    return (
+        <div className="flex p-5 flex-col gap-4">
+            {data.map((item) =>
+                <div
+                    key={item.id}
+                    className="border border-gray-400 p-5 rounded max-w-[400px]"
+                >
+                    <div>{item.attributes.title}</div>
+                    <div>{item.attributes.description}</div>
+                    <img src={item.attributes.preview.data?.attributes.url} alt={""}/>
+                </div>
+            )}
+        </div>
+    );
 }
